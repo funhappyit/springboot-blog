ea731745e9b3a0f3fbc74f7e8b4cc805b86987f9
funhappyit <fun.happyit@gmail.com>
2021-10-04 18:02:54 +0900

    2021-10-04 1) update, delete

--- src/main/java/org/zerock/test/DummyControllerTest.java
+++ src/main/java/org/zerock/test/DummyControllerTest.java
@@ -1,12 +1,22 @@
 package org.zerock.test;
 
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.model.RoleType;
 import org.zerock.model.User;
@@ -18,6 +28,64 @@ public class DummyControllerTest {
 
 	@Autowired //의존성 주입
 	private UserRepository userRepository;
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (Exception e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+	
+		
+		return "삭제되었습니다. id:"+id;
+	}
+	
+	
+	
+	
+	//save함수는 id를 전달하지 않으면 insert를 해주고 
+	//save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고 
+	//email,password
+	 //json 데이터 요청 => java Object(MessageConverter의 Jackson 라이브러리로 변환해서 받음 )
+	@Transactional //함수 종료시에 자동 commit이 됨
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id:"+id);
+		System.out.println("password: "+requestUser.getPassword());
+		System.out.println("email:"+requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+	//	userRepository.save(user);
+		
+		//더팅 체킹 
+		return user;
+	}
+	
+	
+	
+	//http://localhost:8080/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	//한 페이지당 2건의 데이터를 리턴 받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUsers = userRepository.findAll(pageable);
+		
+		//if(pagingUsers.is)
+		List<User> users = pagingUsers.getContent();
+		return users;
+		
+	}
+	
 	
 	@PostMapping("/dummy/join")
 	public String join(User user) {//key=value
