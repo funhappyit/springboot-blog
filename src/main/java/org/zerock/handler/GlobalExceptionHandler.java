6f9cd19ede55cbcf08b7a30b0881aa8b25b675d2
funhappyit <fun.happyit@gmail.com>
2021-10-04 18:36:46 +0900

    2021-10-04 1) exception처리

---
+++ src/main/java/org/zerock/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package org.zerock.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value=Exception.class)
+	public String handleArgumentException(IllegalArgumentException e) {
+		return "<h1>"+e.getMessage()+"</h1>";
+	}
+	
+	
+	
+}
