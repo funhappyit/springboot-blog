88392114d9d375ea25a6017303585376cdbb09ca
funhappyit <fun.happyit@gmail.com>
2021-10-17 17:41:31 +0900

    2021-10-17 1) 스프링 시큐리티 세션 정보 수정

--- src/main/java/org/zerock/controller/api/UserApiController.java
+++ src/main/java/org/zerock/controller/api/UserApiController.java
@@ -2,13 +2,20 @@ package org.zerock.controller.api;
 
 import javax.servlet.http.HttpSession;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.zerock.dto.ResponseDto;
 import org.zerock.model.RoleType;
 import org.zerock.model.User;
@@ -21,8 +28,10 @@ public class UserApiController {
 	private UserService userService;
 	
 	@Autowired
-	private BCryptPasswordEncoder encode;
+	private BCryptPasswordEncoder encoder;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
@@ -34,8 +43,16 @@ public class UserApiController {
 	
 	@PutMapping("/user")
 	public ResponseDto<Integer> update(@RequestBody User user){//key=value, x-www-form-urlencoded
-		
 		userService.userUpdate(user);
+		/*
+		 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 되었지만 
+		 세션값은 변경되지 않은 상태이기 때문에 
+		 우리가 직접 세션값을 변경해줄 것임.
+		 */
+		//세션 등록
+		//String encPassword = encoder.encode(user.getPassword());
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 	
